c6ed68c4f47a727db71c6ed0070c22e29d997ad6
Xiaotian <273420001@qq.com>
2025-01-23 19:24:01 +0800

    [Refine] auto_mode

--- run.py
+++ run.py
@@ -11,7 +11,7 @@ socketio = SocketIO(app)
 # 创建线程池
 executor = ThreadPoolExecutor(max_workers=3)
 
-chess_state = ((8, 8), 3)
+chess_state = ((11, 11), 3)
 (row_len, col_len), power = chess_state # 棋盘大小，power
 
 game = ChessGame(*chess_state)
@@ -25,6 +25,18 @@ monky_ai = MonkyAI()
 minimax_auto = False
 mcts_auto = False
 monky_auto = False
+
+def set_auto_mode(mode=None):
+    global minimax_auto, mcts_auto, monky_auto
+    minimax_auto = False
+    mcts_auto = False
+    monky_auto = False
+    if mode == "minimax":
+        minimax_auto = True
+    elif mode == "mcts":
+        mcts_auto = True
+    elif mode == "monky": 
+        monky_auto = True
 
 def convert_inf_to_string(value):
     if value == float("inf"):
@@ -100,6 +112,7 @@ def handle_redo_move():
 def handle_restart_game():
     game.restart()
     sendDataToBackend(game)
+    set_auto_mode()
 
 
 @socketio.on("minimax_move")
@@ -142,31 +155,19 @@ def handle_monky_move():
 @socketio.on("minimax_auto")
 def handle_minimax_auto():
     # 与Minimax对弈
-    global minimax_auto, mcts_auto, monky_auto
-    minimax_auto = True
-    mcts_auto = False
-    monky_auto = False
-
+    set_auto_mode("minimax")
     handle_minimax_move()
 
 @socketio.on("mcts_auto")
 def handle_mcts_auto():
     # 与MCTS对弈
-    global minimax_auto, mcts_auto, monky_auto
-    minimax_auto = False
-    mcts_auto = True
-    monky_auto = False
-
+    set_auto_mode("mcts")
     handle_mcts_move()
 
 @socketio.on("monky_auto")
 def handle_monky_auto():
     # 与Monky对弈
-    global minimax_auto, mcts_auto, monky_auto
-    minimax_auto = False
-    mcts_auto = False
-    monky_auto = True
-
+    set_auto_mode("monky")
     handle_monky_move()
 
 if __name__ == "__main__":
